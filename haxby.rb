5e417eacfb52042af6f02da37c37820284f307fc
Corry Haines <tabletcorry@gmail.com>
2012-02-12 15:40:37 -0800

    Add dependency on coreutils
    Haxby currently needs the GNU version of readlink

--- haxby.rb
+++ haxby.rb
@@ -4,6 +4,8 @@ class Haxby < Formula
     homepage 'https://github.com/tabletcorry/haxby'
     head "git://github.com/tabletcorry/haxby.git"
 
+    depends_on 'coreutils'
+
     def install
         bin.install ['bin/haxby']
         (share+'haxby').install Dir['lib/*']
