3d9caaee42e18c3fc1ac85603f283fea9d04421c
Corry Haines <tabletcorry@gmail.com>
2012-05-06 18:17:37 -0700

    Add socket mode

---
+++ modes/socket.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+haxby::core::modes::register socket
+haxby::modes::help::register "socket: provide a socket address for contacting postgres"
+
+function haxby::modes::help::socket {
+    echo "usage: haxby socket"
+    echo -e "\ndescription:"
+    echo "provide a connection string to contact postgres"
+}
+
+function haxby::modes::socket {
+    socket=$PG_SOCKET_DIR/.s.PGSQL.$PG_PORT
+    echo $socket
+}
